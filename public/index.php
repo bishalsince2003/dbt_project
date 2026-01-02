143906802d6fbab67dcb6995f9881ad796848e4a
Bishal Kumar <133111722+bishalsince2003@users.noreply.github.com>
2026-01-03 02:56:14 +0530

    fix base href for routing

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+  <base href="/" />
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>DBT Portal — Direct Benefit Transfer</title>
